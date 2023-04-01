2e19e76a225564c7592c5705cfa8a648b6d8392a
Dubin4 <71787435+R1ccoon@users.noreply.github.com>
2023-04-02 02:55:22 +0300

    Add files via upload

--- data/news.py
+++ data/news.py
@@ -14,8 +14,9 @@ class News(SqlAlchemyBase):
     content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+    image = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
-
+    price = sqlalchemy.Column(sqlalchemy.Integer)
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
     user = orm.relationship('User')
