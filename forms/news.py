d0958894f6690afa64d267dea803f2ea865302cf
panaitoff <81642643+panaitoff@users.noreply.github.com>
2023-04-12 20:45:32 +0300

    Add files via upload

--- forms/news.py
+++ forms/news.py
@@ -1,11 +1,16 @@
+import wtforms
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms import BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 
-class NewsForm(FlaskForm):
+class ProductForm(FlaskForm):
     title = StringField('Заголовок', validators=[DataRequired()])
     content = TextAreaField("Содержание")
     is_private = BooleanField("Личное")
+    price = StringField('Цена')
+    image = wtforms.FileField('Изображение')
+    type_k = wtforms.SelectField('Категория товара', choices=("Iphone", "Mac", "Ipad"))
     submit = SubmitField('Применить')
+    is_private = BooleanField("Личное")
